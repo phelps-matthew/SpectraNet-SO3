99ef12c483e0046d1427edd364d34060dcbf4947
mphelps <matthewphelps@odysseyconsult.com>
2022-04-29 21:51:35 -1000

    fix setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="implicit_pdf",
+    description="https://github.com/phelps-matthew/implicit-pdf.git",
+    version="1.0.0",
+    author="",
+    author_email="",
+    packages=setuptools.find_packages(),
+)
